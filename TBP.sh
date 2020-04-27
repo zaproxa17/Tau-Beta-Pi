fe1c927ec7efa758abcf22288b3e2f19f72ee9e5
zaproxa17 <43917334+zaproxa17@users.noreply.github.com>
2020-04-27 10:30:41 -0700

    Bent Challenge

---
+++ TBP.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# ========================================================
+
+echo -n "Enter the #1 Engineering Honor Society: "
+read name
+
+if [ $name == TBP ]; then
+	echo 	"	    *"
+	echo 	"	  *   *"
+	echo	"	    *"
+	echo 	"	   ***"
+	echo 	"	   ***"
+	echo 	"	   ***"
+	echo 	" 	 *******"
+	echo 	" 	 *******"
+	echo	"	  *   *"
+	echo	"	 *     *"
+	echo 	"	*       *"
+	echo	"       *         *"
+	echo	"      *           *"
+	echo 	"     *             *"
+	echo	"    *		    *"
+	echo	" ***********************"
+	echo	" ***********************"
+	echo 	"	  *****"
+	echo 	"	  *****"
+	echo 	"	  *****"
+	echo 	"	  *****"
+	echo 	"	  *****"
+	echo 	"	  *****"
+fi
